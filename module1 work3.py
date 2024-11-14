d00a40d217ef3d9228e3ab156e28f3f5c4966d11
Ekaterina363 <batsl363@gmail.com>
2024-11-14 20:11:18 +0400

    Add files via upload

---
+++ module1 work3.py
@@ -0,0 +1,9 @@
+name= "katy"
+print(name, type(name))
+age = 40
+print(age, type(age))
+new_age = 41
+print(new_age, type(new_age))
+print(age + new_age)
+is_student = True
+print(is_student, type(is_student))
